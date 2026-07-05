40a21a240bb2838cfe8001b29daf003015c41e45
aun_haider <haideraun286@gmail.com>
2026-07-06 01:53:26 +0500

    initial commit - django project setup

---
+++ students/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    father_name = models.CharField(max_length=100)
+    class_name = models.CharField(max_length=20)
+    section = models.CharField(max_length=5)
+    phone = models.CharField(max_length=15)
+    photo = models.ImageField(upload_to='student_photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.student_id}"
