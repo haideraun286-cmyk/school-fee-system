666f923c94b2ad12912010ef8c0862e6862985cb
aun_haider <haideraun286@gmail.com>
2026-08-29 01:31:33 +0500

    dockerized

--- schoolsystem/settings.py
+++ schoolsystem/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mnd$y3bw)$vtp0tf4=)gw_9f-qt5$6k5j$t6v9_#we3idkmiqx'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
